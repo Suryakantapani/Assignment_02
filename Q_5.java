7ff1ed9fe57ac405a78407aeede3a022e3ea7a28
CodeScribe <154777571+Pani-suryakanta-Mohapatra-simon@users.noreply.github.com>
2024-03-13 12:16:41 +0530

    Update Q_5.java

--- Q_5.java
+++ Q_5.java
@@ -6,34 +6,25 @@ class person{
 		name=n;
 		age=a;
 	}
-	void dislay() {
-		System.out.println("name is "+name+"\nage is"+age);
-	}
 }
-	class  employee extends  person{
-		int Id;
-		Double salary;
-	 
-		employee(int Id, double salary,String name, int age){
-			super(name,age);
-			this.Id=Id;
-			this.salary=salary;
-			this.name= name;
-			this.age=age;
-		}
-		 void display() {
-			 System.out.println("name is "+name+"\nage is "+age+"\nid is "+Id+"\nsalary is "+salary+" ");
-		 }
-		
+class  employee extends  person{
+	int Eid;
+	double salary;
+	employee(int Id, double salary,String name, int age){
+		super(name,age);
+		this.Eid=Eid;
+		this.salary=salary;
 	}
+	void empDisplay() {
+		System.out.println("name is "+name+"\nage is "+age+"\nid is "+Eid+"\nsalary is "+salary+" ");
+	}
+		
+}
 
 public class Q_5 {
-
 	public static void main(String[]args) {
-		person sc=new person("gopal",24);
-		sc.dislay();
 		employee sc1=new employee(3,10000000.00,"hari",23);
-		sc1.display();
+		sc1.empDisplay();
 		 
 	}
 }
